d6e5ffcbe9cdf4823910b06a5af3251c825e64c6
Harris Borawski <harrisborawski@me.com>
2013-10-04 13:18:48 -0400

    Added command line arg initialization

--- src/Driver.java
+++ src/Driver.java
@@ -7,6 +7,11 @@
 public class Driver {
 	
 	public static void main(String[] args){
+		int np = Integer.parseInt(args[0]);
+		int nt = Integer.parseInt(args[1]);
+		int tm = Integer.parseInt(args[2]);
+		int em = Integer.parseInt(args[3]);
+		
 		Philosopher p = new Philosopher(1,new Fork(), new Fork(),true,2,(long)500, (long)500);
 		p.run();
 	}
